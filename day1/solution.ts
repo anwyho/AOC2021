200df941ce20126d125443a350474d40c482b5e1
Anthony Ho <anwyho@gmail.com>
2021-12-03 21:34:05 -0800

    Simplify day 1

--- day1/solution.ts
+++ day1/solution.ts
@@ -3,18 +3,16 @@ const sumStrings = (strs: Array<string>): number =>
 
 const part1Expansion = (inputs: Array<string>): string =>
   inputs.filter((_value: string, i: number, array: Array<string>) =>
-    i >= 1 ? parseInt(array[i-1]) < parseInt(array[i]) : false
+    i >= 1 && parseInt(array[i-1]) < parseInt(array[i])
   ).length.toString()
 
 const part2Expansion = (inputs: Array<string>): string =>
   inputs.filter((_value: string, i: number, array: Array<string>) =>
-    i >= 3
-      ? sumStrings(array.slice(i-3,i)) < sumStrings(array.slice(i-2,i+1))
-      : false
+    i >= 3 && sumStrings(array.slice(i-3,i)) < sumStrings(array.slice(i-2,i+1))
   ).length.toString()
 
 // @ts-ignore
-export const part1 = ins => ins.filter((_v, i, arr) => i >= 1 ? parseInt(arr[i-1]) < parseInt(arr[i]) : false).length.toString() 
+export const part1 = ins => ins.filter((_v, i, arr) => i >= 1 && parseInt(arr[i-1]) < parseInt(arr[i])).length.toString() 
 
 // @ts-ignore
-export const part2 = ins => ins.filter((_v, i, arr) => i >= 3 ? arr.slice(i-3,i).reduce((acc, s) => acc + parseInt(s), 0) < arr.slice(i-2,i+1).reduce((acc, s) => acc + parseInt(s), 0) : false).length.toString()
+export const part2 = ins => ins.filter((_v, i, arr) => i >= 3 && arr.slice(i-3,i).reduce((acc, s) => acc + parseInt(s), 0) < arr.slice(i-2,i+1).reduce((acc, s) => acc + parseInt(s), 0)).length.toString()
