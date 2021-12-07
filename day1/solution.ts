3d9ef299b100d73f50954f5d2c611c0623e3dbee
Anthony Ho <anwyho@gmail.com>
2021-12-06 18:58:43 -0800

    Add a line break

--- day1/solution.ts
+++ day1/solution.ts
@@ -3,6 +3,7 @@ export const part1 = ins => ins.filter((_v, i, arr) => i >= 1 && parseInt(arr[i-
 
 // @ts-ignore
 export const part2 = ins => ins.filter((_v, i, arr) => i >= 3 && arr.slice(i-3,i).reduce((acc, s) => acc + parseInt(s), 0) < arr.slice(i-2,i+1).reduce((acc, s) => acc + parseInt(s), 0)).length.toString()
+
 const sumStrings = (strs: Array<string>): number => 
   strs.reduce((acc: number, s: string) => acc + parseInt(s), 0)
 
